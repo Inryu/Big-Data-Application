f50c57c844522dc5284cac16ef31a266177b03be
신인류 <shininryu@MacBookPro.local>
2020-09-26 14:36:04 +0900

    php&mysql example

---
+++ htdocs/phpbook/createtable.php
@@ -0,0 +1,22 @@
+<?php
+
+$mysqli=mysqli_connect("localhost","root","sir104401","testdb");
+
+if(mysqli_connect_errno()){
+    printf("Connect failed : %s\n",mysqli_connect_error());
+    exit();
+}else{
+    $sql="CREATE TABLE testTable(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                                testColumn VARCHAR(75))";
+
+    $res=mysqli_query($mysqli, $sql);
+
+    if($res==TRUE){
+        echo "Table testTable successfully created.";
+    }else{
+        printf("Could not create table: %s\n",mysqli_error($mysqli));
+    }
+
+    mysqli_close($mysqli);
+}
+?>
