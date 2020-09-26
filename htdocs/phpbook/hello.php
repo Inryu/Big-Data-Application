133e91cba20b6655903b4c3e054a11591a4486ed
신인류 <shininryu@MacBookPro.local>
2020-09-26 13:35:35 +0900

    init

---
+++ htdocs/phpbook/hello.php
@@ -0,0 +1,19 @@
+
+<HTML>
+
+
+<TITLE>
+    Hello
+</TITLE>
+
+<BODY>
+Let's PHP 😏
+
+<?php
+        print "Hello World!\n"
+?>
+</BODY>
+
+</HTML>
+
+
---
+++ htdocs/phpbook/receivename.php
@@ -0,0 +1,14 @@
+
+<html>
+<body>
+
+<?php
+    print($_POST['namae']);
+?>
+씨 안녕하세요 🌼
+</body>
+</html>
+
+
+
+
